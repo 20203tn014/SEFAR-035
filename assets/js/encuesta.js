876936d1d9bdf2a774369f78cf063232c85013c1
Rodolfo Campos <20203tn014@utez.edu.mx>
2024-02-21 16:14:20 -0600

    [UPDATE] index.php, encuesta.css, encuesta.js

--- assets/js/encuesta.js
+++ assets/js/encuesta.js
@@ -1,4 +1,95 @@
 window.addEventListener("load", (event) => {
-    let miBody = document.body;
-    miBody.classList.remove("loader");
+    let bodyDocumento = document.body;
+    bodyDocumento.classList.remove("loader");
+    btnAccion((sustraerInt = 0));
 });
+
+function respuesta(idPregunta, idRespuesta) {
+    let classIdPregunta = $(".class_" + idPregunta).removeClass("checkRespuesta");
+    let spanIdPregunta = $("#spanId_" + idPregunta + "_" + idRespuesta).addClass(
+        "checkRespuesta"
+    );
+    let inputRadio = document.querySelector(
+        "#idRespuesta_" + idPregunta + "" + idRespuesta
+    );
+    inputRadio.checked = true;
+
+    contadorChecked();
+}
+
+function contadorChecked() {
+    let valorChecked = [];
+    $("input[type=radio]:checked").each(function () {
+        valorChecked.push(this.value);
+        let totalChecked = valorChecked.length;
+        let totalPreguntasBD = document.querySelector("#totalPreguntasBD");
+        let totalPreguntas = Number(totalPreguntasBD.dataset.totalpreg);
+
+        let porcentaje = (totalChecked * 100) / totalPreguntas;
+        let porcentajeDecimal = Number(porcentaje.toFixed(3));
+        let sustraerInt = Number(porcentaje.toFixed(3).substring(0, 3));
+
+        btnAccion(sustraerInt);
+
+        let barraProgreso = document.querySelector(".progress-bar");
+        barraProgreso.style.width = `${porcentajeDecimal}%`;
+        barraProgreso.textContent = `${sustraerInt}%`;
+    });
+}
+
+function btnAccion(sustraerInt = 0) {
+    let btnEnviar = document.querySelector("#btnEnviar");
+    btnEnviar.disabled = true;
+
+    if (sustraerInt == "100") {
+        btnEnviar.disabled = false;
+        btnEnviar.textContent = "Enviar encuesta";
+    } else {
+        btnEnviar.disabled = true;
+    }
+}
+
+function guardarEncuesta() {
+    let formEncuesta = $("#formEncuesta").serialize();
+    let spanCodigo = document.querySelector("#codigo").textContent;
+    let codigo = Number(spanCodigo);
+
+    // Obtener valores de los campos
+    let nombre = $("#nombre").val();
+    let puesto = $("#puesto").val();
+    let tiempoEmpresa = $("#tiempoEmpresa").val();
+    let centroTrabajo = $("#centroTrabajo").val();
+    let correo = $("#correo").val(); // Nuevo campo
+
+    // Agregar estos valores a los datos que se enviarán
+    let dataSend =
+        formEncuesta +
+        "&codigo=" +
+        codigo +
+        "&nombre=" +
+        nombre +
+        "&puesto=" +
+        puesto +
+        "&tiempoEmpresa=" +
+        tiempoEmpresa +
+        "&centroTrabajo=" +
+        centroTrabajo +
+        "&correo=" +
+        correo; // Nuevo campo
+
+    $.ajax({
+        url: "encuesta_guardada.php",
+        type: "POST",
+        data: dataSend,
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            if (data.respuesta) {
+                alert(
+                    "Las respuestas se han registrado de manera exitosa, ya puede cerrar la encuesta."
+                );
+                location.href = "index.php";
+            }
+        },
+    });
+}
--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" sizes="32x32" href="./assets/img/FaviconR&B.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
@@ -15,7 +14,7 @@
 
     <div id="mainForm">
 
-        <div id="megadiv2">
+        <div id="encuesta">
             <table id="header">
                 <tbody>
                     <tr>
@@ -37,21 +36,22 @@
                     </tr>
                     <tr>
                         <td class="text-center text-bold">NOM-035-STPS-2018 - Guía de Referencia I</td>
-                        <td><small><strong>Tipo:</strong> <?php echo 'Privado'; ?></small></td>
+                        <td><small><strong>Tipo:</strong> <?php echo 'Encuesta'; ?></small></td>
                     </tr>
                 </tbody>
             </table>
 
             <?php
             include('config.php');
-            $sqlPreguntas = ("SELECT * FROM preguntas WHERE status='1'");
-            $query = mysqli_query($connection, $sqlPreguntas);
+            $preguntasBD = ("SELECT * FROM preguntas WHERE status='1'");
+            $query = mysqli_query($connection, $preguntasBD);
 
             $arrayRespuestas = array(
                 "1" => "SI",
                 "0" => "NO",
             );
             ?>
+
             <p id="totalPreguntasBD" data-totalPreg="<?php echo mysqli_num_rows($query); ?>"></p>
 
             <br>
@@ -63,8 +63,8 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label for="nombreCompleto" style="font-size: 16px;">Nombre completo:</label>
-                            <input type="text" class="form-control" id="nombreCompleto" name="nombreCompleto" required>
+                            <label for="nombre" style="font-size: 16px;">Nombre completo:</label>
+                            <input type="text" class="form-control" id="nombre" name="nombre" required>
                         </div>
                     </div>
                     <div class="col-md-6">
@@ -73,22 +73,28 @@
                             <input type="text" class="form-control" id="puesto" name="puesto" required>
                         </div>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-4">
                         <div class="form-group">
                             <label for="tiempoEmpresa" style="font-size: 16px;">Tiempo en la empresa:</label>
                             <input type="text" class="form-control" id="tiempoEmpresa" name="tiempoEmpresa" required>
                         </div>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-4">
                         <div class="form-group">
                             <label for="centroTrabajo" style="font-size: 16px;">Centro de trabajo:</label>
                             <input type="text" class="form-control" id="centroTrabajo" name="centroTrabajo" required>
                         </div>
                     </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="correo" style="font-size: 16px;">Correo electrónico:</label>
+                            <input type="email" class="form-control" id="correo" name="correo" required>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <form id="formFormatoGS" onsubmit="savedEncuesta();return(false);">
+            <br>
+            <form id="formEncuesta" onsubmit="guardarEncuesta();return(false);">
                 <table class="space padding-sm">
                     <thead>
                         <tr id="cabecera">
@@ -111,12 +117,12 @@
                                 <td style="text-align: center;"><?php echo $dataRow['id']; ?></td>
                                 <td style="font-size: 14px;"><?php echo nl2br($dataRow['pregunta']); ?></td>
                                 <td width="150">
-                                    <p class="conteFlex">
+                                    <p class="opciones">
                                         <?php
                                         foreach ($arrayRespuestas as $clave => $valor) { ?>
                                             <span class="class_<?php echo $dataRow['id']; ?>" id="spanId_<?php echo $dataRow['id'] . '_' . $clave; ?>" onclick="respuesta('<?php echo $dataRow['id']; ?>','<?php echo $clave; ?>')">
-                                                <input type="radio" name="respuesta[<?php echo $dataRow['id']; ?>]" id="idResp_<?php echo $dataRow['id'] . $clave; ?>" value="<?php echo $clave; ?>">
-                                                <label for="idResp_<?php echo $dataRow['id'] . $clave; ?>">
+                                                <input type="radio" name="respuesta[<?php echo $dataRow['id']; ?>]" id="idRespuesta_<?php echo $dataRow['id'] . $clave; ?>" value="<?php echo $clave; ?>">
+                                                <label for="idRespuesta_<?php echo $dataRow['id'] . $clave; ?>">
                                                     <?php echo ($valor); ?>
                                                 </label>
                                             </span>
@@ -126,9 +132,9 @@
                             </tr>
                         <?php } ?>
                         <?php
-                        function obtenerNombreGrupo($codigoGrupo)
+                        function obtenerNombreGrupo($ordenGrupo)
                         {
-                            switch ($codigoGrupo) {
+                            switch ($ordenGrupo) {
                                 case 'I':
                                     return 'I.- Acontecimiento traumático severo';
                                 case 'II':
@@ -145,14 +151,10 @@
                     </tbody>
                 </table>
                 <br>
-                <div class="form-group">
-                    <label for="observacion" style="font-size: 16px;">Observaciones</label>
-                    <textarea name="observacion" class="form-control" rows="3">No hay observaciones</textarea>
-                </div>
-                <button type="submit" class="btn btn-primary" id="btnSend">Todas las preguntas deben ser respondidas</button>
+                <button type="submit" class="btn btn-primary" id="btnEnviar">Todas las preguntas deben ser contestadas</button>
             </form>
 
-            <div class="progress">
+            <div class="progreso">
                 <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">
                     0%
                 </div>
